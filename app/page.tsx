60ace78f14f230a6afe2f80257ab01e8448c637c
Ashley Laisure <ashley.laisure@gmail.com>
2025-07-24 11:10:21 -0400

    Assets and Metadata added

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 const page = () => {
     return (
         <div>
-            <h1 className='text-3xl'>Hello Next.js</h1>
-            <p>This is a simple page.</p>
+            <h1 className='text-3xl font-space-grotesk'>Hello Next.js</h1>
+            <p className='font-inter'>This is a simple page.</p>
+            <p className='font-bold'>This is a simple page.</p>
         </div>
     )
 }
+
 
 export default page
